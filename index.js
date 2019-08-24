cca1c0f40c30aa99e178136d7a122b13b4115a40
Michael Sandoval <michaelsandoval8@gmail.com>
2019-08-23 18:31:58 -0700

    minor changes to prepare for removing CSS nth child, and adding array in js to control image urls and div creation

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 $(function(){
     function autoslide(){
         if(!$('.slider .active').is(':last-child')){
-            $('.slider .active').delay(2000).fadeOut(2000, function(){
+            $('.slider .active').delay(3000).fadeOut(2000, function(){
                 $(this).removeClass('active').next().addClass('active').fadeIn();
                 autoslide();
+                console.log(this);
             })
         }else{
-            $('.slider .active').removeClass('active').delay(2000).fadeOut(2000, function(){
+            $('.slider .active').removeClass('active').delay(3000).fadeOut(2000, function(){
                 $('.slider .image').eq(0).addClass('active').fadeOut(2000);
                 autoslide();
             })
