f0424fa4332fdd21264b817181a101d54963588c
Michael Sandoval <michaelsandoval8@gmail.com>
2019-09-25 20:24:56 -0700

    card elements dynamically generated, shuffled, and added to DOM. Click handlers added to the card elements. Element positioning and toggle functions need to be added next.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cardImageURLArray = [
     'assets/images/CardImages/CraterLake1.png',
     'assets/images/CardImages/DeathValley1.png',
     'assets/images/CardImages/GrandCanyon1.png',
-    'assets/images/CardImages/GrandTeton1.png',
+    // 'assets/images/CardImages/GrandTeton1.png',
     'assets/images/CardImages/JoshuaTree1.png',
     'assets/images/CardImages/Seqoia1.png',
     'assets/images/CardImages/SmokyMountains1.png',
@@ -15,7 +15,7 @@ const parkImageURLArray = [
     'assets/images/MatchImages/CraterLakeImage600.png',
     'assets/images/MatchImages/DeathValley600.png',
     'assets/images/MatchImages/GrandCanyon600.png',
-    'assets/images/MatchImages/GrandTeton600.png',
+    // 'assets/images/MatchImages/GrandTeton600.png',
     'assets/images/MatchImages/JoshuaTreeImage600.png',
     'assets/images/MatchImages/SeqoiaImage600.png',
     'assets/images/MatchImages/SmokyMountainsImage600.png',
@@ -23,95 +23,74 @@ const parkImageURLArray = [
     'assets/images/MatchImages/YosemiteImage600.png'
 ];
 
-//holds references as the dom elements are created
 const DOMElements = {};
-
 
 $(document).ready(function(){
     createCards(cardImageURLArray);
+    assignClickHandlers();
 
 });
 
-const createCards = (cardImages) => {
-    let elementsArray = [];
+const assignClickHandlers = () => {
+    
+    $('.card-row').on('click', '.card-container', function(event){
+        console.log('this on click: ', this);
+        console.log('event.currenttarget on click: ', event.currentTarget);
+        $($(event.currentTarget).children()[0]).addClass('hidden');
+    })
+}
 
-    for(let i = 0; i < 9; i++){
+const createCards = (cardImages) => {
+    cardImages = cardImages.concat(cardImages);
+
+    let flipCardsArr = [];
+    for(let i = 0; i < cardImages.length; i++){
         let cardContainer = $("<div>").addClass('card-container');
-        let cardBack = $("<div>").addClass('card-back');
+        let cardBack = $("<div>").addClass('card-back').css('background-image', `url('assets/images/CardImages/CardBack1.png')`);
         let cardReverse = $("<div>").addClass('card-reverse').css('background-image', `url('${cardImages[i]}')`);
         cardContainer.append(cardBack, cardReverse);
-        let cardDouble = cardContainer.clone(true, true);
-        // let cardDuplicate = JSON.parse(JSON.stringify(cardContainer));
-        // console.log('cardDuplicate: ', cardDuplicate);
-        elementsArray.push(cardContainer, cardDouble);
+        flipCardsArr.push(cardContainer);
     }
 
-    // let deepCopyElements = JSON.parse(JSON.stringify(elementsArray));
-    // console.log('deepCopyElements: ', deepCopyElements);
-
-    // let doubledCardsArr = elementsArray.concat([...elementsArray]);
-    // let doubledCardsArr = elementsArray.concat(deepCopyElements);
-    
-    // let doubledCardsArr = elementsArray.clone(true, true);
-    console.log('elementsArray: ', elementsArray);
-    console.log('doubledCardsArr: ', doubledCardsArr);
-    
-
-    DOMElements.cards = elementsArray;
+    DOMElements.cards = flipCardsArr;
     DOMElements.cardRows = [
         $('.card-row1'), 
         $('.card-row2'), 
         $('.card-row3')
     ];
-    console.log('DOMElements: ', DOMElements);
-    console.log('DOMElements.cards: ', DOMElements.cards);
 
-    //must double cards before appending, and add shuffle feature
-    for(let i = 0; i < doubledCardsArr.length; i++){
-        console.log('card append loop i: ', i);
-        // switch(i){
-        //     case 0:
-        //     case 1: 
-        //     case 2:
-        //     case 3:
-        //     case 4:
-        //     case 5: DOMElements.cardRows[0].append(DOMElements.cards[i]);
-        //         break;
-        //     case 6:
-        //     case 7:
-        //     case 8:
-        //     case 9:
-        //     case 10:
-        //     case 11: DOMElements.cardRows[1].append(DOMElements.cards[i]);
-        //         break;
-        //     case 12:
-        //     case 13:
-        //     case 14:
-        //     case 15:
-        //     case 16:
-        //     case 17: DOMElements.cardRows[2].append(DOMElements.cards[i]);
-        //         break;
-        // }
-        // switch(true){
-        //     case i < 6: DOMElements.cardRows[0].append(DOMElements.cards[i]);
-        //         break;
-        //     case i < 12: DOMElements.cardRows[1].append(DOMElements.cards[i]);
-        //         break;
-        //     case i < 18: DOMElements.cardRows[2].append(DOMElements.cards[i]);
-        //         break;
-        // }
-        console.log('current card: ', DOMElements.cards[i]);
-        DOMElements.cardRows[0].append(DOMElements.cards[i]);
-    }
-    console.log('DOMElements.cardRows[0]', DOMElements.cardRows[0]);
-    console.log('DOMElements.cardRows[1]', DOMElements.cardRows[1]);
-    console.log('DOMElements.cardRows[2]', DOMElements.cardRows[2]);
-
+    shuffleCardsArr();
+    appendCardsToDom();
 }
 
+const shuffleCardsArr = () => {
+    let m = DOMElements.cards.length;
+    let t;
+    let i;
 
+    while (m) {
+        i = Math.floor(Math.random() * m--);
 
+        t = DOMElements.cards[m];
+        DOMElements.cards[m] = DOMElements.cards[i];
+        DOMElements.cards[i] = t;
+    }
+}
 
+const appendCardsToDom = () => {
+    for (let i = 0; i < DOMElements.cards.length; i++) {
+        console.log('card append loop i: ', i);
+
+        switch (true) {
+            case i < 6: DOMElements.cardRows[0].append(DOMElements.cards[i]);
+                break;
+            case i < 12: DOMElements.cardRows[1].append(DOMElements.cards[i]);
+                break;
+            case i < 18: DOMElements.cardRows[2].append(DOMElements.cards[i]);
+                break;
+        }
+    }
+}
 
 
 
